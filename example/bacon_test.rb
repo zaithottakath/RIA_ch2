d404af764a8d9ab19d932435f02f90e1ba9af77a
zaithottakath <zaithottakath@gmail.com>
2013-03-05 17:16:32 -0800

    This is chapter 2. The examples folder and the bacon folder that were in the examples.

---
+++ example/bacon_test.rb
@@ -0,0 +1,16 @@
+#TDD Testing.
+#red-gren-refactoring
+
+require 'test/unit'
+
+class Bacon
+  def self.saved?
+    true
+  end
+end
+
+class BaconTest < Test::Unit::TestCase
+  def test_saved
+    assert Bacon.saved?, "Our bacon was not saved :-("
+  end
+end
---
+++ example/example_test.rb
@@ -0,0 +1,9 @@
+
+
+require 'test/unit'
+
+class ExampleTest < Test::Unit::TestCase
+  def truth
+    assert 1
+  end
+end
